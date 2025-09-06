10cd9c84f642fbfd1ec552450ff91111b77086b9
Divyanshu Bisht <divyanshubisht5734@gmail.com>
2025-09-06 12:13:58 +0530

    06-09-2025

--- index.js
+++ index.js
@@ -50,4 +50,25 @@ app.listen(4000 , ()=>{
 
 // RabbitMQ *************************
 
-// it is a message broker which is used to connect all different microservices for asynchronous communications.
+// it is a message broker which is used to connect all different microservices for asynchronous communications. 
+
+// RabbitMQ helps in asynchoronus communication like we have different services in their folder for eg users , captains , and rides .   
+// now for creating there must be a users . we have to check is user available or not . we get the user data from user through axios i.e is synchronous communications . means now ride totally depends on users if somehow users service fall down then ride service also fall down .
+
+// now we have users data also now create ride and ride has been created now we have to send this ride detail to captain service. so that captain can know the pickup and destination point . since we can not send the ride detail directly to captain so we use rabbitMQ.
+
+// to use RabbitMQ we have to installed one library in all services i.e.  npm i amqplib 
+// then get a url from the free website   ( cloudampq )   paste the url inside .env file.
+
+// important step :- we have to connect all service with RabbitMQ .
+
+// RabbitMQ is connected to the ride , user , captain individual . now rabbitMQ is connected to all the services . if any service want or send the data to any services . service will  publishtoqueue i.e. RabbitMQ then RabbitMQ send the data to the services.
+
+// for eg we have create a ride then we have to send the ride detail to  the captain then in ride service will publishtoqueue . then where we want to send the data in this case in captain will recieve that data through the function subscribetoqueue 
+
+// note :- publishtoqueue and subscribetoqueue is function inside rabbit.js . rabbit.js is function which will connect all service with rabbitMQ individual it means this rabbit.js function created in all services.
+
+// Long polling 
+
+// Long polling is a technique where the client sends a request and the server holds the connection open until new data is available or a timeout occurs, thereby reducing the number of empty requests.
+
